5541ca8aa28931f1c492510cc1a5fd3544a0f2fb
tclaes <tom.claes82@gmail.com>
2021-01-02 00:14:32 +0100

    tracks mock data update

--- src/app/tracks/tracks.component.ts
+++ src/app/tracks/tracks.component.ts
@@ -12,29 +12,13 @@ import { Observable } from 'rxjs';
 })
 export class TracksComponent implements OnInit {
 
-  beers$: Observable<any>;
 
   constructor(
-    private title: Title,
-    private meta: Meta,
-    private http: HttpClient
+
   ) { }
 
   ngOnInit() {
-    const api = this.http.get('https://api.punkapi.com/v2/beers/random');
-    this.beers$ = api.pipe(
-      share(),
-      concatAll(),
-      first());
-    this.beers$.subscribe( beer => {
-      this.meta.updateTag({ property: 'og:title', content: beer.name });
-    });
 
-    this.title.setTitle('PMC Cycling');
-
-    this.meta.addTags([
-      { property: 'twitter:card', content: 'summary'},
-      { property: 'og:url', content: '/tracks'},
-    ]);
   }
+
 }
