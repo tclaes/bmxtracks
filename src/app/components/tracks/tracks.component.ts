849dab5cfaa09d660046b3983fa4adc151b37b78
Tom Claes <tom.claes82@gmail.com>
2021-01-11 15:31:28 +0100

    Feature/import content (#14)
    * sorting added
    
    * content imported, refactor to service
    
    * changed default trackLink

--- src/app/components/tracks/tracks.component.ts
+++ src/app/components/tracks/tracks.component.ts
@@ -1,14 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import gql from 'graphql-tag';
-import {QueryServiceService} from '../../services/query-service.service';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-
-export interface Track {
-  trackName: string;
-  trackImage: string;
-  trackLink: string;
-}
+import {Track} from '../../general/types/types';
+import {TrackService} from './track.service';
 
 @Component({
   selector: 'app-tracks',
@@ -17,39 +10,9 @@ export interface Track {
 })
 export class TracksComponent implements OnInit {
   allTracks$: Observable<Track[]>;
-  constructor( private dataService: QueryServiceService) {}
+  constructor( private trackService: TrackService) {}
 
   ngOnInit() {
-    const query = gql`
-        {
-          allTracks {
-            edges {
-              node {
-                track,
-                trackImage,
-                link {
-                  __typename
-                  ... on _ExternalLink{
-                    url
-                  }
-                },
-                location
-              }
-            }
-          }
-        }
-      `;
-
-    this.allTracks$ = this.dataService.getAllTracks(query).pipe(
-      map(result => {
-        return result.map(val => {
-          return {
-            trackName: val.node.track[0].text,
-            trackImage: (val.node?.trackImage) ? val.node.trackImage.Thumb.url : null,
-            trackLink: (val.node?.link) ? val.node.link.url : 'https://bmxtracks.netlify.app'
-          };
-        });
-      })
-    );
+    this.allTracks$ = this.trackService.getAllTracks();
   }
 }
---
+++ src/app/general/types/types.ts
@@ -0,0 +1,6 @@
+export interface Track {
+  trackName: string;
+  trackImage: string;
+  trackContent: {};
+  trackLink: string;
+}
